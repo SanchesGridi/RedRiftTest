964d7726d45f180b50a354d735efc02c2caa4cd1
AlexGrid <sanchesgridi@gmail.com>
2023-06-09 09:51:56 +0300

    remove std templated pages

---
+++ RedRiftTest/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace RedRiftTest.Extensions;
+
+public static class WebApplicationBuilderExtensions
+{
+    public static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder @this)
+    {
+        var services = @this.Services;
+
+        services.AddRazorPages();
+        services.AddServerSideBlazor();
+
+        return @this;
+    }
+}
---
+++ RedRiftTest/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace RedRiftTest.Extensions;
+
+public static class WebApplicationExtensions
+{
+    public static WebApplication ConfigureMiddlewares(this WebApplication @this)
+    {
+        if (!@this.Environment.IsDevelopment())
+        {
+            @this.UseExceptionHandler("/Error");
+            @this.UseHsts();
+        }
+        @this.UseHttpsRedirection();
+        @this.UseStaticFiles();
+        @this.UseRouting();
+
+        return @this;
+    }
+
+    public static WebApplication ConfigureRoutes(this WebApplication @this)
+    {
+        @this.MapBlazorHub();
+        @this.MapFallbackToPage("/_Host");
+
+        return @this;
+    }
+}
