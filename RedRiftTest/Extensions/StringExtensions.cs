6f641f21e2e80ccdc77bc15098fcda3a7a733ebe
AlexGrid <sanchesgridi@gmail.com>
2023-06-09 11:11:40 +0300

    update error handler

---
+++ RedRiftTest/Extensions/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace RedRiftTest.Extensions;
+
+public static class StringExtensions
+{
+    public static bool IsEmpty(this string @this)
+    {
+        return string.IsNullOrWhiteSpace(@this);
+    }
+}
--- RedRiftTest/Pages/Error.cshtml.cs
+++ RedRiftTest/Pages/Error.cshtml.cs
@@ -4,20 +4,10 @@ using System.Diagnostics;
 
 namespace RedRiftTest.Pages
 {
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    [IgnoreAntiforgeryToken]
+    [IgnoreAntiforgeryToken, ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
     {
         public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-
-        private readonly ILogger<ErrorModel> _logger;
-
-        public ErrorModel(ILogger<ErrorModel> logger)
-        {
-            _logger = logger;
-        }
 
         public void OnGet()
         {
