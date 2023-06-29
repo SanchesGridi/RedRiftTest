0f8fee48958b0a6a9739f24ca9e32d399fa87edc
AlexGrid <sanchesgridi@gmail.com>
2023-06-29 09:18:08 +0300

    add popup logic

---
+++ RedRiftTest/wwwroot/js/text-editor.js
@@ -0,0 +1,10 @@
+﻿export function openDialog(id) {
+    document.getElementById(id).showModal();
+}
+
+export function closeDialog(id) {
+    const dialog = document.getElementById(id);
+    if (dialog.open) {
+        dialog.close();
+    }
+}
